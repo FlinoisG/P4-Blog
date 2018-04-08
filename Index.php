54b1acd5eab094493f2a7f3856b3797716433482
FlinoisG <flinois.gauthier@gmail.com>
2018-04-08 16:53:28 +0200

    added index.php, base,php and css

--- Index.php
+++ Index.php
@@ -1,1 +1,46 @@
+<?php
 
+require_once "vendor/autoload.php";
+
+use App\Controller\PostController;
+use App\Controller\DefaultController;
+use App\Repository\PostRepository;
+
+
+if(isset($_GET['p'])){
+    $routeTemp = explode('.', $_GET['p']);
+    if (count($routeTemp) === 2){
+        $routeTemp = [
+            "controller" => $routeTemp[0],
+            "action" => $routeTemp[1]
+        ];
+    }
+} else {
+    $routeTemp = [
+        "controller" => "",
+        "action" => ""
+    ];
+}
+
+$getParams = isset($_GET['params']) ? $_GET['params'] : null;
+$postParams = isset($_POST['params']) ? $_POST['params'] : null;
+$params = [
+    "get" => $getParams,
+    "post" => $postParams
+];
+
+$controller = "\\App\\Controller\\" . ucfirst($routeTemp['controller']) . "Controller";
+if (class_exists($controller, true)){
+    $controller = new $controller();
+    if (in_array($routeTemp["action"], get_class_methods($controller))){
+        call_user_func_array([$controller, $routeTemp["action"]], $params);
+    } else {
+       
+        $content = 'Erreur 404';
+        require('base.php');
+    }
+} else {
+    $content = 'Erreur 500';
+    require('base.php');
+    
+}
---
+++ base.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="favicon.ico">
+
+        <title><?= $title ?></title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link rel="stylesheet" type="text/css" href="css/base.css">
+    </head>
+
+    <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="/public/?p=post.index">Blog</a>
+        <?php
+        
+        if (isset($_SESSION['auth'])){
+            echo '<a class="btn btn-outline-danger btn-logout" href="/public/?p=post.index&logout=true">Déconnextion</a>';
+        }
+        ?>
+    </nav>
+
+    <main role="main" class="container">
+        <div class="row">
+            <?= $content ?>
+        </div>
+    </main>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></script>
+    </body>
+</html>
